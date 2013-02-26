a202523983d64aaad9e7015b0987297a209587d6
E John Feig <ejohn@feigdev.com>
2013-02-26 09:22:54 -0800

    cleanup

--- problem1.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-
-import webapp2
-
-class PSOneHandler(webapp2.RequestHandler):
-    def get(self):
-        self.response.write('Hello, Udacity!')
-
-main=('/ps1', PSOneHandler)
